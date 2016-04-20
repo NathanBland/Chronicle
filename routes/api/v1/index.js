1614f36cf716eb4a86ddd1cd5b278f13602b2d75
Nathan Bland <nathan.bland@gmail.com>
2016-04-20 11:26:15 -0600

    updates to core api to return endpoints. Split Api into its own repository.

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -8,6 +8,12 @@ exports.setup = function (app, express) {
   router.use('/auth', auth.setup(app, express))
   router.use('/user', user.setup(app, express))
   router.use('/entry', journal.setup(app, express))
-
+  router.route('/')
+    .get(function (req, res, next) {
+      return res.status(200).json({
+        'message': 'Welcome to the Api',
+        'endpoints': ['/auth', '/user', '/entry']
+      })
+    })
   return router
 }
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 var routes = require('./routes/')
-var sass = require('node-sass-middleware')
 var app = express()
 
 app.set('dbhost', '127.0.0.1')
@@ -17,19 +16,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
-
-app.set('view engine', 'jade')
-app.use(
-  sass({
-    root: __dirname,
-    indentedSyntax: true,
-    src: '/sass',
-    dest: '/public/css',
-    prefix: '/css',
-    debug: true
-  })
-)
-app.use(express.static('public'))
 
 app.use(routes.setup(app, express))
 
